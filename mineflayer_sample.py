c79cb26979c93986c567ef98a840cfcc48961642
danmartinez78 <danmartinez78@gmail.com>
2021-09-27 01:26:29 -0500

    Update mineflayer_sample.py

--- mineflayer_sample.py
+++ mineflayer_sample.py
@@ -1,12 +1,20 @@
 from javascript import require, On, Once, AsyncTask, once, off
 
 mineflayer = require('mineflayer')
+pathfinder = require('mineflayer-pathfinder')
+mineflayerViewer = require('prismarine-viewer').mineflayer
+
+
 
 random_number = id([]) % 1000 # Give us a random number upto 1000
 BOT_USERNAME = f'colab_{random_number}'
 
+
+
 bot = mineflayer.createBot({ 'host': '192.168.0.229', 'port': 25566, 'username': BOT_USERNAME, 'hideErrors': False })
 
-# The spawn event 
-once(bot, 'login')
-bot.chat('I spawned')
+@On(bot, 'spawn')
+def onspawn(*a):
+    mineflayerViewer(bot, { 'firstPerson': True, 'port': 3000 })
+    print('server started on *:3000')
+
